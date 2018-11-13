b54efaca1d631c92f2443c8a014abb0d1191c252
junifar <junifar@gmail.com>
2018-11-13 16:34:23 +0700

    Adding department_realisasi

--- src/main/kotlin/com/prasetia/erp/pojo/department/DepartmentSummary.kt
+++ src/main/kotlin/com/prasetia/erp/pojo/department/DepartmentSummary.kt
@@ -4,7 +4,8 @@ class DepartmentSummary(
         val id:Long,
         val periode:Long?,
         val nilai_budget:Long?,
+        val realisasi_budget:Long?,
         val persent_budget:Float?
 ){
-    constructor(): this(0,0,0,0f)
+    constructor(): this(0,0,0,0,0f)
 }
