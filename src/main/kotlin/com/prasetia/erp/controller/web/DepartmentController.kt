160aedd81b85a8b08cb4ecef34f8080ecee42ad2
sriaprianti98 <sriaprianti98@gmail.com>
2018-11-13 16:50:48 +0700

    sri:department summary edit

--- src/main/kotlin/com/prasetia/erp/controller/web/DepartmentController.kt
+++ src/main/kotlin/com/prasetia/erp/controller/web/DepartmentController.kt
@@ -1,14 +1,24 @@
 package com.prasetia.erp.controller.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.prasetia.erp.constant.GlobalConstant
+import com.prasetia.erp.constant.GlobalConstant.Companion.BASE_URL
+import com.prasetia.erp.pojo.department.DepartmentSummary
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import java.net.URL
 
 @Controller("Budget Department Controller")
 class DepartmentController{
 
     @RequestMapping("/budget_department")
     fun indexDepartment(model: Model):String{
+        val objectMapper = ObjectMapper()
+        val url = URL(BASE_URL + "api/department_summary")
+        val departmentSummaryDataList: List<DepartmentSummary> = objectMapper.readValue(url)
+        model.addAttribute("departmentSummaryDataList", departmentSummaryDataList)
         return "department/index"
     }
 }
--- src/main/kotlin/com/prasetia/erp/pojo/department/DepartmentSummary.kt
+++ src/main/kotlin/com/prasetia/erp/pojo/department/DepartmentSummary.kt
@@ -1,10 +1,11 @@
 package com.prasetia.erp.pojo.department
 
 class DepartmentSummary(
-        val id:Long,
+        val id:Long?,
         val periode:Long?,
         val nilai_budget:Long?,
+        val realisasi_budget:Long?,
         val persent_budget:Float?
 ){
-    constructor(): this(0,0,0,0f)
+    constructor(): this(0,0,0,0,0f)
 }
