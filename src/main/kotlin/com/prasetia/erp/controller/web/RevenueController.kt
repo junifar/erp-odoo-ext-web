2e5a8e67c9d38a6e1d5688afd5d2cff6b16e01cc
junifar <junifar@gmail.com>
2019-03-05 16:13:38 +0700

    Add new module
    - Revenue Report

---
+++ src/main/kotlin/com/prasetia/erp/controller/web/RevenueController.kt
@@ -0,0 +1,24 @@
+package com.prasetia.erp.controller.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller("Revenue Web Controller")
+class RevenueController{
+    @RequestMapping("/revenue")
+    fun index(model: Model):String{
+        return "revenue/index"
+    }
+
+    @RequestMapping("/revenue/{tahun}")
+    fun revenueByYear(model: Model, @PathVariable("tahun") tahun:String):String{
+        return "revenue/revenue_by_year"
+    }
+
+    @RequestMapping("/revenue/{tahun}/{project_type}")
+    fun revenueByYearType(model: Model, @PathVariable("tahun") tahun: String, @PathVariable("project_type") project_type:String):String{
+        return "revenue/revenue_by_year_project_type"
+    }
+}
